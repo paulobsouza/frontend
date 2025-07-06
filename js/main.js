f0ba9ae4d1545965a5a9363e0cd8bcd04d12b435
Paulo Souza <101979170+paulobsouza@users.noreply.github.com>
2025-07-06 18:40:03 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const app = new AppController(); 
+    const api = new ApiService('https://backend-yr71.onrender.com/api');
+    
+    const app = new AppController(api);
+
     app.init();
 });
