7c57e9aa22a31fa60d9e5b0f9fb050d4e636578e
Paulo Souza <101979170+paulobsouza@users.noreply.github.com>
2025-07-06 16:40:06 -0300

    Create Alimento.js

---
+++ js/models/Alimento.js
@@ -0,0 +1,10 @@
+class Alimento {
+  constructor(id, nome, carboidratos, proteinas, gorduras, calorias) {
+    this.id = id;
+    this.nome = nome;
+    this.carboidratos = carboidratos;
+    this.proteinas = proteinas;
+    this.gorduras = gorduras;
+    this.calorias = calorias;
+  }
+}
